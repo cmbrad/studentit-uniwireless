1bfc4257b9bcebcbab714e798252607f6d900de6
StudentIT <studentit@unimelb.edu.au>
2016-07-22 15:02:07 +1000

    make web work

--- sit-wifi/bin/web.py
+++ sit-wifi/bin/web.py
@@ -1,17 +1,44 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify
+
+from lib.check_wireless import student_can_access_uniwireless
+from lib.cross_origin import crossdomain
+
 
 app = Flask(__name__)
 
 staff_username = os.environ.get('LDAP_STAFF_USERNAME')
 staff_password = os.environ.get('LDAP_STAFF_PASSWORD')
 
-@app.route("/")
-def hello():
-    _, student_id, access = student_can_access_uniwireless(staff_username, staff_password, student_username)
-    return access
+
+@app.route('/')
+def home():
+    return jsonify({
+        'error': 'Not a valid endpoint'
+    })
+
+
+@app.route("/<student_username>")
+@crossdomain(origin='*')
+def check_wireless(student_username):
+    try:
+        if not staff_username or not staff_password:
+            raise Exception('Server misconfiguration. LDAP credentials not set.')
+        _, student_id, access, groups = student_can_access_uniwireless(staff_username, staff_password, student_username)
+    except Exception as e:
+        return jsonify({
+            'error': e.message
+        })
+
+    return jsonify({
+        "username": student_username,
+        "id": student_id,
+        "access": access,
+        "groups": groups
+    })
 
 
 if __name__ == "__main__":
+    app.debug = True
     app.run(host='0.0.0.0', port=5000)
 
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,13 @@
-import sys
+#!/usr/bin/env python
 
-sys.path.insert(0, '/var/www/check_wifi')
-from bin.web import app as application
+import os, sys
+
+sys.path.insert(0, '/apps/studentit-uniwireless/sit-wifi')
+#from bin.web import app as application
+
+def application(environ, start_response):
+    for key in ['LDAP_STAFF_USERNAME', 'LDAP_STAFF_PASSWORD']:
+        os.environ[key] = environ.get(key, '')
+    from bin.web import app as _application
+
+    return _application(environ, start_response)
